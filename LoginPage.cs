a2411527f94758cde8a1c4a249bf882e6f9852ef
[Denys] <[denis.gordovkiy@gmail.com]>
2025-11-18 19:28:27 +0200

    HomePage

---
+++ LoginPage.cs
@@ -0,0 +1,37 @@
+using Microsoft.Playwright;
+using NUnit.Framework;
+
+namespace TheConnectedShop
+{
+    public class LoginPage
+    {
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
+        private const string ExpectedUrl = "https://keatech-live-staging-frontend.azurewebsites.net/login";
+        private const string ExpectedTitle = "Keatech Live - Login or get new password";
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            var context = await _browser.NewContextAsync();
+            _page = await context.NewPageAsync();
+        }
+        [Test]
+        public async Task OpenHomePageTest()
+        {
+            await _page.GotoAsync(ExpectedUrl);
+            string currentUrl = _page.Url;
+            Assert.That(currentUrl,Is.EqualTo(ExpectedUrl), "Url page is not incorrect");
+
+            string currentTitle = await _page.TitleAsync();
+            Assert.That(currentTitle,Is.Not.Empty, "page is not title");
+            Assert.That(currentTitle, Is.EqualTo(ExpectedTitle), $"Заголовок сторінки очікувався '{ExpectedTitle}', але отримано '{currentTitle}'");
+        }
+    }
+}
